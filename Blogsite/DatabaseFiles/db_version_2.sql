0bb868a79954884107106d16cf0a07120f7d1afc
Fredrik Stigsson <info@annytab.se>
2015-01-21 13:44:28 +0100

    Fixed category listing and paging bug

--- Blogsite/DatabaseFiles/db_version_2.sql
+++ Blogsite/DatabaseFiles/db_version_2.sql
@@ -1,12 +1,24 @@
 ﻿BEGIN TRANSACTION;
 
 /* STATIC TEXTS */
-INSERT INTO dbo.static_texts (id, language_id, value) VALUES ('translate_user_details',1,'Du måste uppdatera (översätta) ditt författarnamn och din beskrivning för att kunna betygsätta och kommentera den här posten!');
-INSERT INTO dbo.static_texts (id, language_id, value) VALUES ('translate_user_details',2,'You need to update (translate) your author name and your description to be able to rate and comment this post!');
+IF NOT EXISTS (SELECT * FROM dbo.static_texts WHERE id = 'translate_user_details' AND language_id = 1)
+BEGIN
+INSERT INTO dbo.static_texts (id, language_id, value) VALUES ('translate_user_details',1,'Du måste uppdatera (översätta) ditt författarnamn och din beskrivning för att kunna betygsätta och kommentera den här posten!')
+END;
+IF NOT EXISTS (SELECT * FROM dbo.static_texts WHERE id = 'translate_user_details' AND language_id = 2)
+BEGIN
+INSERT INTO dbo.static_texts (id, language_id, value) VALUES ('translate_user_details',2,'You need to update (translate) your author name and your description to be able to rate and comment this post!')
+END;
 
 /* WEBSITE SETTINGS */
-INSERT INTO dbo.website_settings (id, value) VALUES ('SEND-EMAIL-USE-SSL','false');
-INSERT INTO dbo.website_settings (id, value) VALUES ('REDIRECT-HTTPS','false');
+IF NOT EXISTS (SELECT * FROM dbo.website_settings WHERE id = 'SEND-EMAIL-USE-SSL')
+BEGIN
+INSERT INTO dbo.website_settings (id, value) VALUES ('SEND-EMAIL-USE-SSL','false')
+END;
+IF NOT EXISTS (SELECT * FROM dbo.website_settings WHERE id = 'REDIRECT-HTTPS')
+BEGIN
+INSERT INTO dbo.website_settings (id, value) VALUES ('REDIRECT-HTTPS','false')
+END;
 
 /* EXCECUTE THE TRANSACTION */
 COMMIT;
