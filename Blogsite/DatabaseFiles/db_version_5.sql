3dda676850d61acbc0d0884cb3a6e9c23342912c
Fredrik Stigsson <info@annytab.se>
2015-08-05 11:20:48 +0200

    author paging and cookie consent

---
+++ Blogsite/DatabaseFiles/db_version_5.sql
@@ -0,0 +1,14 @@
+﻿BEGIN TRANSACTION;
+
+/* COOKIE CONSENT */
+IF NOT EXISTS (SELECT * FROM dbo.static_texts WHERE id = 'cookie_consent' AND language_id = 1)
+BEGIN
+INSERT INTO dbo.static_texts (id, language_id, value) VALUES ('cookie_consent',1,'Cookies hjälper oss att tillhandahålla våra tjänster. Genom att använda våra tjänster samtycker du till att vi använder cookies. Läs mer i vår sekretesspolicy.')
+END;
+IF NOT EXISTS (SELECT * FROM dbo.static_texts WHERE id = 'cookie_consent' AND language_id = 2)
+BEGIN
+INSERT INTO dbo.static_texts (id, language_id, value) VALUES ('cookie_consent',2,'Cookies help us deliver our services. By using our services you agree to our use of cookies. Read more in our Privacy Policy.')
+END;
+
+/* EXCECUTE THE TRANSACTION */
+COMMIT;
